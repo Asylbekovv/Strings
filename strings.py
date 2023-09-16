044c466cc643cc186cc3bb72d17fea62a0deeca0
Asylbekovv <144426976+Asylbekovv@users.noreply.github.com>
2023-09-16 23:00:07 +0600

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,19 @@
+my_name = "Arstan"
+print(my_name)
+print(type(my_name))
+print(id(my_name))
+mesage_msg = """This is long text for python 
+and this very cool variante for people 
+"""
+print(mesage_msg)
+
+long_str = """This is long str"""
+print(type(long_str))
+
+
+my_str = "This is random text"
+print(len(my_str))
+print(my_str.replace("text","word"))
+print(my_str)
+print(my_str[1:6])
+print(my_str.count("te"))
